5ec19386404342484aad749042a6d197a3cb23e8
Urielsousa <urielzinho@hotmail.com>
2026-07-27 17:33:05 -0300

    att total de tarefas

--- todo-smart/src/pages/Dashboard.jsx
+++ todo-smart/src/pages/Dashboard.jsx
@@ -18,5 +18,12 @@ export default function Dashboard () {
             </div>
 
             {/*Cards */}
-            <div className= 
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mb-8'>
+                <div className='bg-gray-900 p-5 rounded-2xl border border-gray-800'>
+                    <p className='text-gray-400 text-sm'> 
+                        Total de tarefas:
+                    </p>
+                    <h2 className='text-3xl font-bold mt-2'>0</h2>
+                </div>
+            </div>
     )
