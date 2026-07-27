5175935968b371e4e232043881d2e3e230e2abb0
Urielsousa <urielzinho@hotmail.com>
2026-07-27 17:20:14 -0300

    todo-smart

---
+++ todo-smart/src/pages/Dashboard.jsx
@@ -0,0 +1,22 @@
+import {FaPlus} from 'react-icons/fa'
+
+export default function Dashboard () {
+    return (
+        <div className='min-h-screen bg-gray-950 text-white p-6'>
+            {/* Header */}
+
+            <div className='flex items-center justify-between mb-8' >
+        <div>
+            <h1 className='text-3xl font-bold1'>TO DO Smart 🚀</h1>
+            <p className='text-gray-400'>Suas tarefas + Google Agenda + whatsApp</p>
+            </div>
+
+            <button className='bg-green-500 hover:bg-green-600 transition px-4 py-2 rounded-xl flex items-center gap-2 fonte-medium'>
+                <FaPlus />
+                Adicionar nova tarefa
+            </button>
+            </div>
+
+            {/*Cards */}
+            <div className= 
+    )
